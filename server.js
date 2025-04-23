2027281bbf4f8ace1f3dc63bce57425383efb9fe
Joris Falter <joris@jorisfalter.com>
2025-04-23 13:49:51 +0200

    x

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post("/slack/events", async (req, res) => {
   if (event && event.type === "message" && !event.bot_id) {
     const slackUserId = event.user;
     const user = await User.findOne({ slackUserId });
+    console.log("DM received:", event.text, "from user:", event.user);
 
     if (!user) {
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/calendar.events&access_type=offline&prompt=consent&state=${slackUserId}`;
